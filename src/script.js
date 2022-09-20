b23d7b8e5eaaa2d7956f54f4bcefec9831235197
CyraIftikhar <112282875+CyraIftikhar@users.noreply.github.com>
2022-09-20 13:57:57 +0100

    fixed spacing in CSS

--- src/script.js
+++ src/script.js
@@ -5,7 +5,7 @@ let date = now.getDate();
 let hours = now.getHours();
 let minutes = now.getMinutes();
 if (hours < 10) {
-  hours = `0${minutes}`;
+  hours = `0${hours}`;
 }
 if (minutes < 10) {
   minutes = `0${minutes}`;
